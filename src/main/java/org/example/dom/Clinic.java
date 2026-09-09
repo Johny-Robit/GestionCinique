b1de11c00a5f8e5070ab145e31444c6998b34a1d
Johny-Robit <jean.robitaille.2@ulaval.ca>
2026-09-09 19:39:34 -0400

    Debut

---
+++ src/main/java/org/example/dom/Clinic.java
@@ -0,0 +1,13 @@
+package org.example.dom;
+
+public class Clinic {
+    public Clinic(/* ... */) {
+    }
+
+    public void triagePatient(String name, int gravity, VisibleSymptom visibleSymptom) {
+        // TODO
+    }
+
+    // D'autres méthodes peuvent être nécessaires
+
+}
