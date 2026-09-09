d644fa96275c14fc4075f7840c306c0b03e04a12
Johny-Robit <jean.robitaille.2@ulaval.ca>
2026-09-09 19:50:21 -0400

    Debut 2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+import org.example.dom.Clinic;
+import org.example.dom.TriageType;
+import org.example.dom.VisibleSymptom;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Ceci n'est pas un test!! C'est un exemple d'utilisation.
+        TriageType doctorTriageType = TriageType.FIFO;
+        TriageType radiologyTriageType = TriageType.FIFO;
+
+        // Clinic clinic = new Clinic(/* , ... */)
+        // clinic.triagePatient("John", 4, VisibleSymptom.MIGRAINE);
+    }
+}
